286d20c1712739d5e3f5f79ba58a12f4fd07dc83
Toma Nikolov <toma.nikolov@telerik.com>
2017-06-06 15:29:20 +0300

    Add git credential helper for code commit.

---
+++ resources/git/aws-credential-helper/code-commit-credential-helper.js
@@ -0,0 +1,44 @@
+const aws4 = require('aws4');
+
+const SERVICE = 'codecommit'
+const METHOD = 'GIT'
+
+const credential = {
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || process.env.AWS_SECRET_KEY,
+	sessionToken: process.env.AWS_SESSION_TOKEN,
+}
+
+let stdin = ''
+process.stdin.on('data', chunk => {
+	stdin += chunk.toString();
+});
+
+process.stdin.on('end', () => {
+	const options = parseParams(stdin);
+	writeGitParameters(options)
+});
+
+function writeGitParameters(options) {
+	let username = credential.accessKeyId;
+	if (credential.sessionToken) {
+		username += `%${credential.sessionToken}`;
+	}
+
+	options.service = SERVICE;
+	options.method = METHOD;
+	const signer = new aws4.RequestSigner(options, credential);
+	const password = `${signer.getDateTime()}Z${signer.signature()}`
+
+	console.log(`username=${username}`);
+	console.log(`password=${password}`);
+}
+
+function parseParams(stdin) {
+	const params = stdin.split('\n').map(param => param.replace(/\w*=/, ''));
+	return {
+		protocol: params[0],
+		host: params[1],
+		path: params[2]
+	};
+}
