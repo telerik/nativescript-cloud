80442e83f6a04efd327e4a33401e20b4205e0cdc
Toma Nikolov <toma.nikolov@telerik.com>
2017-06-06 15:33:08 +0300

    Add code commit api service.

--- lib/constants.ts
+++ lib/constants.ts
@@ -19,9 +19,10 @@ export const PROVISION_TYPES = {
 	DEVELOPMENT: "Development",
 };
 
-export const AUTH_SERVICE_NAME = "auth-service";
-export const BUILD_SERVICE_NAME = "build-service";
-export const EMULATORS_SERVICE_NAME = "emulators-service";
+export const AUTH_SERVICE_NAME = "auth";
+export const BUILD_SERVICE_NAME = "build";
+export const EMULATORS_SERVICE_NAME = "emulators";
+export const CODE_COMMIT_SERVICE_NAME = "code-commit";
 
 export const CLOUD_BUILD_EVENT_NAMES = {
 	BUILD_OUTPUT: "buildOutput"
---
+++ lib/definitions/cloud-services/code-commit-service.d.ts
@@ -0,0 +1,27 @@
+interface ICodeCommitService {
+	getRepository(appId: string): Promise<IGetRepositoryResponse>;
+	deleteRepository(appId: string): Promise<IDeleteRepositoryResponse>;
+}
+
+interface IGetRepositoryResponse {
+	cloneUrlHttp: string;
+	cloneUrlSsh: string;
+	repositoryName: string;
+	credentials: ICodeCommitCredentials;
+	isNewRepository: boolean;
+}
+
+interface IRemoteUrl {
+	httpRemoteUrl: string;
+}
+
+interface ICodeCommitCredentials {
+	accessKeyId: string;
+	expiration: string;
+	secretAccessKey: string;
+	sessionToken: string;
+}
+
+interface IDeleteRepositoryResponse {
+	repositoryId: string;
+}
