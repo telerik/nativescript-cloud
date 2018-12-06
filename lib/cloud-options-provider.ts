c586ad18da681fffa8cf1a6a9b4274cab9cc44b4
Kristian Dimitrov <kddimitrov@progress.com>
2018-12-06 18:11:22 +0200

    chore: add mandatory hasSensitiveValue field for cloud options

--- lib/cloud-options-provider.ts
+++ lib/cloud-options-provider.ts
@@ -3,15 +3,15 @@ import { DEFAULT_ANDROID_PUBLISH_TRACK } from "./constants";
 export class CloudOptionsProvider implements ICloudOptionsProvider {
 	public get dashedOptions() {
 		return {
-			accountId: { type: OptionType.String },
-			apiVersion: { type: OptionType.String },
-			local: { type: OptionType.Boolean },
-			serverProto: { type: OptionType.String },
-			namespace: { type: OptionType.String },
-			sharedCloud: { type: OptionType.Boolean },
-			workflow: { type: OptionType.Object },
-			vmTemplateName: { type: OptionType.String },
-			track: { type: OptionType.String, default: DEFAULT_ANDROID_PUBLISH_TRACK },
+			accountId: { type: OptionType.String, hasSensitiveValue: true },
+			apiVersion: { type: OptionType.String, hasSensitiveValue: false },
+			local: { type: OptionType.Boolean, hasSensitiveValue: false },
+			serverProto: { type: OptionType.String , hasSensitiveValue: true},
+			namespace: { type: OptionType.String, hasSensitiveValue: true },
+			sharedCloud: { type: OptionType.Boolean, hasSensitiveValue: false },
+			workflow: { type: OptionType.Object, hasSensitiveValue: true },
+			vmTemplateName: { type: OptionType.String, hasSensitiveValue: false },
+			track: { type: OptionType.String, default: DEFAULT_ANDROID_PUBLISH_TRACK, hasSensitiveValue: false },
 		};
 	}
 }
