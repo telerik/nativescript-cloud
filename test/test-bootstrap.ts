051ea0a5139da5eafa8a17886217dc82c86c22db
rosen-vladimirov <rosen.vladimirov@progress.com>
2019-08-14 12:07:56 +0300

    fix: tests should be able to run
    Currently unit tests are not running as the test-bootstrap requires `errors`, which is using the `deprecated` decorator. The decorator is evaluated when the errors file is required and it expects to have a logger in the globally registered injector.
    However, we do not have it in the tests. Remove require of the errors in the test-bootstrap, as it is not needed there.

--- test/test-bootstrap.ts
+++ test/test-bootstrap.ts
@@ -6,7 +6,3 @@ cliGlobal._ = require("lodash");
 cliGlobal.$injector = require("nativescript/lib/common/yok").injector;
 
 use(require("chai-as-promised"));
-
-// Converts the js callstack to typescript
-import errors = require("nativescript/lib/common/errors");
-errors.installUncaughtExceptionListener();
